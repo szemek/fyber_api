4a6dc4b9945840a1117df62195515a25681f5996
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2015-11-15 13:57:47 +0100

    ignore codeclimate.com when making requests to external services

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -28,6 +28,7 @@ CodeClimate::TestReporter.start
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
+  config.ignore_hosts 'codeclimate.com'
 end
 
 RSpec.configure do |config|
